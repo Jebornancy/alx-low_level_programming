3e7859ed90d04eb081d7e7bb7c4f2ac8198b6b51
Jebornancy <nancykibon@gmail.com>
2023-02-23 15:36:45 -0800

    main.h

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,4 @@
-#ifndef main_h
-#define main_h
-
+int _putchar(char character);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -10,7 +8,7 @@ void more_numbers(void);
 void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
+void fizz_buzz(int range);
 void print_triangle(int size);
+void prime_factors(long test_num);
 void print_number(int n);
-
-#endif
