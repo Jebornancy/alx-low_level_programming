9d0082fd9a1cbdafac627f724bcd94f8371b6b3c
Jebornancy <nancykibon@gmail.com>
2023-03-10 12:05:18 -0800

    mynameis

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: count of the arguments
+ * @argv: array of pointers to the string
+ * Return: always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc * 0]);
+
+	return (0);
+}
