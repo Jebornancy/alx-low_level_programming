644aabc5a4e26f49d63a5c31770378940e96a7f8
Jebornancy <nancykibon@gmail.com>
2023-04-10 04:45:39 -0700

    function that sets the value of a bit to 0 at a given index

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -10,11 +10,11 @@
  * Return: 1 if it worked -1 otherwise
  */
 int clear_bit(unsigned long int *n, unsigned int index)
-{       
+{
         if (index >= sizeof(unsigned long int) * 8){
 		return (-1);
 }
 	*n &= ~(1UL << index);
 	return (1);
-}       
+}
 
